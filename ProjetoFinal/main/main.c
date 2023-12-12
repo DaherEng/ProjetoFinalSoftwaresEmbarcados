3b71f6c14817704c75074903a2d67da7f49e4ed0
DaherEng <igordahercabral@hotmail.com>
2023-12-12 09:18:37 -0300

    'estruturacao'

--- ProjetoFinal/main/main.c
+++ ProjetoFinal/main/main.c
@@ -2,12 +2,18 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/message_buffer.h"
+#include "freertos/semaphore.h"
 #include <LiquidCrystal.h>
 #include <dht11.h>
+#include <Wire.h>
+#include <math.h>
+#include <TinyGPS++.h>
 
 #define GPIO_DHT
+#define MPU 0x68 // Endereço I2C do sensor
 
 MessageBufferHandle_t bufferTemperatura, bufferUmidade, bufferLatitude, bufferLongitude, bufferVelocidade, bufferCombustivel;
+SemaphoreHandle_t mutexRFID;
 
 void TaskAtualizaDisplay();
 void TaskReadGPS();
@@ -26,6 +32,8 @@ void app_main(void)
     bufferVelocidade = xMessageBufferCreate(128);
     bufferCombustivel = xMessageBufferCreate(128);
 
+    mutexRFID = xSemaphoreCreateMutex();
+
     xTaskCreatePinnedToCore(TaskFormataDados, "Task Formata", 4098, NULL, 2, NULL, 1);
     xTaskCreatePinnedToCore(TaskLeituraRFID, "Task RFID", 4098, NULL, 3, NULL, 1);
     xTaskCreatePinnedToCore(TaskAtualizaDisplay, "Task Display", 4098, NULL, 1, NULL, 0);
@@ -33,6 +41,28 @@ void app_main(void)
     xTaskCreatePinnedToCore(TaskReadDHT, "Task DHT", 4098, NULL, 1, NULL, 1);
     xTaskCreatePinnedToCore(TaskReadMPU, "Task MPU", 4098, NULL, 1, NULL, 1);
     xTaskCreatePinnedToCore(TaskReadCombustivel, "Task Combustivel", 4098, NULL, 1, NULL, 1);
+}
+
+void TaskLeituraRFID()
+{
+    int usuarioID;
+    int usuarioCredenciado;
+
+    while (1)
+    {
+        xSemaphoreTake(mutexRFID, portMAX_DELAY);
+        if (deucerto pegar o semaforo)
+        {
+            // Lê o RFID
+            if (usuarioID==usuarioCredenciado)
+            {
+                // Libera as outras tasks
+                vTaskDelete(NULL);
+            }
+        }
+        
+    }
+    
 }
 
 void TaskReadDHT()
@@ -89,13 +119,53 @@ void TaskReadCombustivel()
 
 void TaskReadMPU()
 {
-    // mpu init
+    // Variáveis para armazenar os dados coletados
+    float AccX;
+    float AccY;
+    float AccZ;
+    float VelX_inicial = 0;
+    float VelY_inicial = 0;
+    float VelZ_inicial = 0;
+    // Velocidade Real
+    float velocidade;
+    // Inicialização do MPU-6050
+    Wire.begin();
+    Wire.beginTransmission(MPU);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+    // Configuração do fundo de escala do acelerômetro (2G)
+    Wire.beginTransmission(MPU);
+    Wire.write(0x1C);
+    Wire.write(0x00);
+    Wire.endTransmission(true);
+
 
     while (1)
     {
-        // mpu read
+        Wire.beginTransmission(MPU);
+        Wire.write(0x3B); // Endereço de inicio da solicitação de dados
+        Wire.endTransmission(false);
+        Wire.requestFrom(MPU, 14, true); // Solicita 14 bytes de dados a partir do primeiro endereço
+
+        // Armazena os dados
+        AccX = Wire.read() << 8 | Wire.read();
+        AccY = Wire.read() << 8 | Wire.read();
+        AccZ = Wire.read() << 8 | Wire.read();
+
+        // Integração de Newton
+        VelX = VelX_inicial + (AccX / (16384 * 0.1));
+        VelY = VelY_inicial + (AccY / (16384 * 0.1));
+        VelZ = VelZ_inicial + (AccZ / (16384 * 0.1));
+
+        velocidade = sqrt((VelX * VelX) + (VelY * VelY) + (VelZ * VelZ));
         
-        xMessageBufferSend(bufferVelocidade, &combustivel, sizeof(int), portMAX_DELAY);
+        VelX_inicial = VelX;
+        VelY_inicial = VelY;
+        VelZ_inicial = VelZ;
+
+        
+        xMessageBufferSend(bufferVelocidade, &velocidade, sizeof(float), portMAX_DELAY);
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
@@ -116,7 +186,7 @@ void TaskAtualizaDisplay()
         xMessageBufferReceive(bufferUmidade, &umidade, sizeof(int), portMAX_DELAY);
         xMessageBufferReceive(bufferLatitude, &latitude, sizeof(int), portMAX_DELAY);
         xMessageBufferReceive(bufferLongitude, &longitude, sizeof(int), portMAX_DELAY);
-        xMessageBufferReceive(bufferVelocidade, &velocidade, sizeof(int), portMAX_DELAY);
+        xMessageBufferReceive(bufferVelocidade, &velocidade, sizeof(float), portMAX_DELAY);
         xMessageBufferReceive(bufferCombustivel, &combustivel, sizeof(int), portMAX_DELAY);
 
         sprintf(buffer1, "T:%d H:%d V:%d C:%d", temperatura, umidade, velocidade, combustivel);
